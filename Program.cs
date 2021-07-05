2a64790cac6155ce9144744841a7030e6366f2c5
Eric Kuijpers <egm.kuijpers1@avans.nl>
2021-07-05 10:32:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WK1LotsOfProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wk1 = new Week1Properties();
+            wk1.SetReadAndWritePropertyWithMethods("Hallo");
+            wk1.ReadAndWriteProperty = " ";
+            wk1.ReadAndWritePropertyFull = "Wereld";
+            wk1.ReadAndWritePropertyFullLambda = "!";
+            Console.WriteLine($"{wk1.GetReadAndWritePropertyWithMethods()}{wk1.ReadAndWriteProperty}{wk1.ReadAndWritePropertyFull}{wk1.ReadAndWritePropertyFullLambda}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week1Properties.cs
@@ -0,0 +1,49 @@
+﻿namespace WK1LotsOfProperties
+{
+    public class Week1Properties
+    {
+        private string readAndWritePropertyWithMethods;
+        private string readAndWritePropertyFullLambda;
+        private string readAndWritePropertyFull;
+
+        /// <summary>
+        /// Separate Set method for a private variable with room for validation
+        /// </summary>
+        /// <returns></returns>
+        public string GetReadAndWritePropertyWithMethods()
+        {
+            return readAndWritePropertyWithMethods;
+        }
+
+        /// <summary>
+        /// Separate Set method for a private variable with room for validation
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetReadAndWritePropertyWithMethods(string value)
+        {
+            readAndWritePropertyWithMethods = value;
+        }
+        /// <summary>
+        /// Simple property with automatic getter and setter. No private variable necessary, it's magic
+        /// </summary>
+        public string ReadAndWriteProperty { get; set; }
+
+        /// <summary>
+        /// Property with lambda expressions (week 2 material), returns and modifies private variable.
+        /// Lambda expressions can be extended with more code, but make sure code retains readability
+        /// </summary>
+        public string ReadAndWritePropertyFullLambda { get => readAndWritePropertyFullLambda; set => readAndWritePropertyFullLambda = value; }
+
+        /// <summary>
+        /// sets and returns a private variable, with room for validation in setter and room for formatting in getter;
+        /// </summary>
+        public string ReadAndWritePropertyFull {
+            get {
+                return readAndWritePropertyFull;
+            }
+            set {
+                readAndWritePropertyFull = value;
+            }
+        }
+    }
+}
